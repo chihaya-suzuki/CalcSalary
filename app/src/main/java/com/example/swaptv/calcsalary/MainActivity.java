600cdd79f8f76fa1eadc639fdb5980aeab0698fd
Chihaya Suzuki <yes.wave@gmail.com>
2017-07-10 11:41:16 +0900

    add overTimeWork part.

--- app/src/main/java/com/example/swaptv/calcsalary/MainActivity.java
+++ app/src/main/java/com/example/swaptv/calcsalary/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.swaptv.calcsalary;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.ParseException;
@@ -19,20 +22,27 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 private TextView mResultView;
+private CheckBox mOverTimeCheckBox;
 private Handler mHandler;
 
 private double mMonthSalary;
 private double mMillSecSalary;
 private long mDateTimeFrom;
 private long mDateTimeTo;
+private long mOverTimeDiff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 給与表示View
         mResultView = (TextView)findViewById(R.id.resultView);
+        // ボタンリスナー登録
         findViewById(R.id.sampleButton).setOnClickListener(this);
+        // 残業中チェックボックス
+        mOverTimeCheckBox = (CheckBox)findViewById(R.id.overTimeCheckBox);
+        mOverTimeCheckBox.setOnClickListener(this);
     }
 
     @Override
@@ -53,14 +63,26 @@ private long mDateTimeTo;
         int id = v.getId();
         switch(id) {
             case R.id.sampleButton:
+                // 設定画面遷移
                 Intent intent = new Intent(this, SettingActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.overTimeCheckBox:
+                if(mOverTimeCheckBox.isChecked())
+                    // 残業中なら赤文字
+                    mResultView.setTextColor(Color.RED);
+                else {
+                    // 通常は黒文字
+                    mResultView.setTextColor(Color.BLACK);
+                }
                 break;
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // !!! ここは期待通りに動いていない !!!
+        // !!! 設定ボタンのアイコンが表示されない !!!
         getMenuInflater().inflate(R.menu.menu_app, menu);
         return true;
     }
@@ -70,6 +92,7 @@ private long mDateTimeTo;
         int id = item.getItemId();
 
         switch (id) {
+            // 設定画面に遷移
             case R.id.action_settings:
                 Intent intent = new Intent(this, SettingActivity.class);
                 startActivity(intent);
@@ -88,22 +111,41 @@ private long mDateTimeTo;
         mResultView.setText(text);
     }
 
-    private void calcTime() {
+    private void calcSalaryNow() {
         // 現在時刻取得
         Calendar nowCalendar = Calendar.getInstance();
-
         // 現在時刻をミリセカンドに変換
         long dateTimeNow = nowCalendar.getTimeInMillis();
-
         // 勤務時間中なら計算
         long diff = 0;
+        // 勤務開始時間を過ぎているか
         if (mDateTimeFrom < dateTimeNow) {
+            // 勤務終了時間より前か
             if (mDateTimeTo > dateTimeNow) {
+                if(mOverTimeCheckBox.isEnabled()) {
+                    mOverTimeCheckBox.setChecked(false);
+                    mOverTimeCheckBox.setEnabled(false);
+                }
+                // 現在給与計算
                 diff = dateTimeNow - mDateTimeFrom;
             } else {
-                diff = mDateTimeTo - mDateTimeFrom;
+                // 残業中の処理
+                // チェックボックスがDisabledならEnabledに変更
+                if(!mOverTimeCheckBox.isEnabled()) {
+                    mOverTimeCheckBox.setEnabled(true);
+                }
+                // チェックが入っているなら残業代計算
+                if(mOverTimeCheckBox.isChecked()) {
+                    mOverTimeDiff = (long)((dateTimeNow - mDateTimeTo) * 1.25);
+                }
+                // 残業代追加
+                diff = mDateTimeTo - mDateTimeFrom + mOverTimeDiff;
             }
+            // ビューに現在給与セット
             setResultView((long) (diff * mMillSecSalary) + " 円");
+        } else {
+            // 勤務開始前
+            setResultView("0 円");
         }
     }
 
@@ -121,29 +163,35 @@ private long mDateTimeTo;
         return workDayCount;
     }
 
+    // 1ミリセカンド分の給与計算
     private double calcMillSecSalary() {
         return mMonthSalary / (calcWorkDay() * (mDateTimeTo - mDateTimeFrom));
     }
 
+    // 給与計算開始処理
     private void excuteCalc() {
         mMonthSalary = Double.parseDouble(SettingPrefUtil.getMonthSalary(this));
         mMillSecSalary = calcMillSecSalary();
 
+        // 給料計算ループ開始(500msec毎に計算)
+        // ディレイをかけないとCPUへの負荷が半端なくエミュを動かしてたMacが悲鳴を上げた
         mHandler = new Handler();
         final Runnable r = new Runnable() {
             @Override
             public void run() {
-                calcTime();
+                calcSalaryNow();
                 mHandler.postDelayed(this, 500);
             }
         };
         mHandler.post(r);
     }
 
+    // 給与計算中止
     private void stopCalc() {
         mHandler.removeCallbacksAndMessages(null);
     }
 
+    // プリファレンスの値を時刻にセット
     private Calendar setCalendar(String time) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time.substring(0,2)));
